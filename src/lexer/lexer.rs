17c2d00cc7755e27c674ff798b534302d7ec5319
Ryan Faulhaber <ryf@sent.as>
2023-08-27 14:55:50 -0400

    some lexer work

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,6 +1,15 @@
+use super::{scanner::Scanner, token::Token};
+
 #[derive(Debug)]
-pub(super) struct Lexer<'l> {
+pub(crate) struct Lexer<'l> {
     source: &'l str,
+    scanner: Scanner<'l>,
 }
 
-impl<'l> Iterator for Lexer<'l> {}
+impl<'l> Iterator for Lexer<'l> {
+    type Item = Token<'l>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!();
+    }
+}
--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,2 +1,3 @@
+pub mod lexer;
 mod scanner;
 mod token;
